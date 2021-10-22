38866f3afa3e1c95a5047389d11c4bd3d8e3b550
MKS1998 <nksayyad2900@outlook.com>
2021-10-22 18:04:35 +0530

    Added : Program For The Edit Contact By The First Name

--- src/com/addressbook/AddressBookMain.java
+++ src/com/addressbook/AddressBookMain.java
@@ -1,9 +1,19 @@
 package com.addressbook;
 
+import java.util.Scanner;
+
 public class AddressBookMain {
 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
 		System.out.println("Welcome to the Address Book Problem..");
 		NewAddressBooks person = new NewAddressBooks();
 		person.addContact();
+		System.out.println("Enter Y To Edit The Contact");
+		String str = sc.nextLine();
+		if (str.equals("y") || str.equals("Y")) {
+			person.editContact(); // Calling The Edit Contact Method
+			System.out.println("You have Entered following data");
+			System.out.println("The Contact Details After Editing : " + person);
+		}
 	}
 }
